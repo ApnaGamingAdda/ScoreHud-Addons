77403e2e592adc95e61af92a41d952a87bbdf51e
ApnaGamingAdda <63424572+ApnaGamingAdda@users.noreply.github.com>
2020-04-10 11:08:57 +0530

    UpdateBasic.phar
    Added money

--- BasicAddon.php
+++ BasicAddon.php
@@ -24,24 +24,13 @@ namespace JackMD\ScoreHud\Addons
 				"{name}"	       => $player->getName(),
 				"{real_name}"          => $player->getName(),
 				"{display_name}"       => $player->getDisplayName(),
+                                "{money}"              => $player->getMoney(),
 				"{online}"             => count($player->getServer()->getOnlinePlayers()),
 				"{max_online}"         => $player->getServer()->getMaxPlayers(),
-				"{item_name}"          => $player->getInventory()->getItemInHand()->getName(),
-				"{item_id}"            => $player->getInventory()->getItemInHand()->getId(),
-				"{item_meta}"          => $player->getInventory()->getItemInHand()->getDamage(),
-				"{item_count}"         => $player->getInventory()->getItemInHand()->getCount(),
 				"{x}"                  => intval($player->getX()),
 				"{y}"                  => intval($player->getY()),
 				"{z}"                  => intval($player->getZ()),
-				"{load}"               => $player->getServer()->getTickUsage(),
-				"{tps}"                => $player->getServer()->getTicksPerSecond(),
-				"{level_name}"         => $player->getLevel()->getName(),
-				"{level_folder_name}"  => $player->getLevel()->getFolderName(),
-				"{ip}"                 => $player->getAddress(),
-				"{ping}"               => $player->getPing(),
 				"{time}"               => date($this->getScoreHud()->getConfig()->get("time-format")),
-				"{date}"               => date($this->getScoreHud()->getConfig()->get("date-format")),
-				"{world_player_count}" => count($player->getLevel()->getPlayers())
 			];
 		}
 	}
